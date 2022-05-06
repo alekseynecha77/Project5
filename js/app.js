a8d1617373e5736b2d085640853aa13eebd75c99
alekseynecha77 <nechaev.alexxx23@gmail.com>
2022-05-05 23:47:01 -0400

    updating

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-baguetteBox.run('.gallery');
-
-const search = new Filter('search', 'data-caption');
+function search_image() {
+    let input = document.getElementById('search').value
+    input=input.toLowerCase();
+    let x = document.getAttribute('img');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
